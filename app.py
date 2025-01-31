2e6db84068438dd3e9976c58b40d6c117cc3e996
aryan <ramtekearyan1805@gmail.com>
2025-02-01 00:19:53 +0530

    new route/endpoint

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app=Flask(__name__)
 @app.route("/info")
 def lw():
     return "Hello World...\nbye bye!!"
+
+@app.route("/me")
+def lw():
+    return "Aryan Ramteke...\n"
 app.run(host="0.0.0.0", port=5000)
